15140c227a395c95264cbd968a360ceb0abe6d47
Ricardo Felgueiras <armindofelgueiras@gmail.com>
2021-05-03 14:37:32 +0100

    Add log with the json alert to be created

--- opsGenie.js
+++ opsGenie.js
@@ -8,6 +8,8 @@ function createAlert(issuePayload, url, apiKey) {
 
     var create_alert_json = getJson(issuePayload.issue);
 
+    console.log(create_alert_json);
+
     opsGenie.alertV2.create(create_alert_json, function (error, alert) {
         if (error) {
             console.error(error);
