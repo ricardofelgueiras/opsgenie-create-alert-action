e536486fee45ec3f592a7a0b477c83f88e46d141
Ricardo Felgueiras <armindofelgueiras@gmail.com>
2021-05-04 18:17:02 +0100

    Add improvements

--- index.js
+++ index.js
@@ -8,17 +8,13 @@ try
     // const api_key = core.getInput('api-key');
     // const url = core.getInput('opsgenie-api-url');
     const github_token = core.getInput('github-token');
-
-    console.log(context.issue.repo);
-    console.log(context.issue.issue_number);
-    console.log(context.issue.owner);
     
     const octokit = github.getOctokit(github_token);
     const issue = (async () => {
                     return await octokit.issues.get({
-                        repo: context.repo,
-                        issue_number: context.issue_number,
-                        owner: context.owner
+                        repo: context.issue.repo,
+                        issue_number: context.issue.number,
+                        owner: context.issue.owner
                     })
                 })();
 
